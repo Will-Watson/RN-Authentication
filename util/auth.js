8eb962e13d085723ab5e9412c906d2377abda92f
William Watson <williampwatson1991@gmail.com>
2024-02-03 23:34:15 -0500

    fix: insane inssue with expo and .env

--- util/auth.js
+++ util/auth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const createUser = async (email, password) => {
   try {
     const response = await axios.post(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.API_KEY,
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.EXPO_PUBLIC_API_KEY,
       {
         email,
         password,
