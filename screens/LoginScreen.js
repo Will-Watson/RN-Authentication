b48e2d5b3fcbc753a066a5b09f53a66f6fd412ec
William Watson <williampwatson1991@gmail.com>
2024-02-03 23:45:06 -0500

    feat: login working well

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from '../components/Auth/AuthContent';
+import { login } from '../util/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const loginHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    try {
+      const response = await login(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message='Loggin In....' />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
 
+export const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${process.env.EXPO_PUBLIC_API_KEY}`;
+
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+};
+
 export const createUser = async (email, password) => {
   try {
-    const response = await axios.post(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.EXPO_PUBLIC_API_KEY,
-      {
-        email,
-        password,
-        returnSecureToken: true,
-      }
-    );
+    const response = await authenticate('signUp', email, password);
   } catch (error) {
     throw new Error(error);
   }
 };
+
+export const login = async (email, password) => {
+  try {
+    const response = await authenticate('signInWithPassword', email, password);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
