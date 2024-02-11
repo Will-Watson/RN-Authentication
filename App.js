f6636bdeb8600ba4d0ffb9a5d2d771a316b61b39
William Watson <williampwatson1991@gmail.com>
2024-02-10 20:10:11 -0500

    feat: logout button added to authenticated stack header

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
 import { Colors } from './constants/styles';
+import IconButton from './components/ui/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -27,6 +28,8 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authContext = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -35,7 +38,20 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name='Welcome' component={WelcomeScreen} />
+      <Stack.Screen
+        name='Welcome'
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon='exit'
+              color={tintColor}
+              size={24}
+              onPress={() => authContext.logout()}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
