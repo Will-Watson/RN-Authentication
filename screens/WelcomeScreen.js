11c36c3fca3b1d8c198a89037002f7a51ff9b350
William Watson <williampwatson1991@gmail.com>
2024-02-10 20:24:51 -0500

    feat: added an protected route that displays a message only if a user is authenticated with database modifications with firebase and the addtion of an auth querie to the route being requested

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,32 @@
+import axios from 'axios';
+import { useEffect, useState, useContext } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState(null);
+
+  const authContext = useContext(AuthContext);
+  const token = authContext.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://rn-authentication-a3781-default-rtdb.firebaseio.com/message.json?auth=' +
+          token
+      )
+      .then((response) => {
+        console.log(response.data);
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
